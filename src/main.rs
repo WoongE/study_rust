778de101e004f4eb3a7ba41884a3e618b7d95fb0
Wonung Kim <woong17@naver.com>
2022-04-16 12:41:24 +0900

    standard input & output

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+    // ! 는 매크로란 의미다. 따라서, println 은 지금 함수가 아니라 매크로다.
+    println!("Guess the number!");
+    println!("Please input your guess.");
+    let mut guess = String::new();
+    stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 }
